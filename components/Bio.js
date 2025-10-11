65d5dbfb856729acf9f7cb4fd825269e19cbfab7
Talha Aleem <2023cs638@student.uet.edu.pk>
2025-10-11 06:10:35 +0500

    msg

--- components/Bio.js
+++ components/Bio.js
@@ -23,15 +23,13 @@ const Bio = () => {
           data-aos-delay="200"
         >
           <p className="mb-3 lg:mb-6">
-            Based in Sydney, Australia, I am a professional <strong>Actor, Model, Presenter, and Content
-            Creator</strong> with over 20 years of experience across the international entertainment industry. My career has taken me from film and television sets to photo studios, live stages, radio stations, and digital platforms—each offering new opportunities to perform, connect, and create.
+            Based in Sydney, Australia, I am a professional <strong className="bio-strong">Actor, Model, Presenter, and Content Creator</strong> with over 20 years of experience across the international entertainment industry. My career has taken me from film and television sets to photo studios, live stages, radio stations, and digital platforms—each offering new opportunities to perform, connect, and create.
           </p>
           <p className="mb-3 lg:mb-6">
-            My passion for performance began early, singing in my school choir and taking part in yearly stage productions. That early love of storytelling evolved into a lifelong pursuit of the craft. To refine my skills, I’ve studied at some of the most respected acting schools and institutions both locally and internationally, including <strong>National Institute of Dramatic
-            Art (NIDA), Australian Film and Television Academy (TAFTA), The Actors Centre
-            (Sydney), Victorian College of the Arts (VCA), Sydney Drama School, Rogues West
-            Acting School (Vancouver, Canada), and Royal Academy of Dramatic Art (RADA,
-            London).</strong>
+            Over the years, I have worked extensively across film, television, modelling, and presenting for both stage and screen, including radio and community television. Alongside numerous commercial campaigns within Australia and abroad, I’ve been fortunate to collaborate with exceptional creative teams and production companies around the world.
+          </p>
+          <p className="mb-3 lg:mb-6">
+            I have commercially represented some of the world’s most recognised brands—including <strong className="bio-strong">Qantas, Jetstar, BMW, RSPCA, Cancer Council, Car Australia, DiDi, ING, Chemist Warehouse, Luxaflex, OzCar,</strong> and many more—both online and on air.
           </p>
         </div>
 
@@ -43,13 +41,13 @@ const Bio = () => {
           data-aos-delay="400"
         >
           <p className="mb-3 lg:mb-6">
-            Over the years, I have worked extensively across film, television, modelling, and presenting for both stage and screen, including radio and community television. Alongside numerous commercial campaigns within Australia and abroad, I’ve been fortunate to collaborate with exceptional creative teams and production companies around the world. I have commercially represented some of the world’s most recognised brands—including <strong>Qantas, Jetstar,
-            BMW, RSPCA, Cancer Council, Car Australia, DiDi, ING, Chemist Warehouse, Luxaflex,
-            OzCar,</strong> and many more—both online and on air.
+            My passion for performance began early, singing in my school choir and taking part in yearly stage productions. That early love of storytelling evolved into a lifelong pursuit of the craft. To refine my skills, I’ve studied at some of the most respected acting schools and institutions both locally and internationally, including <strong className="bio-strong">National Institute of Dramatic Art (NIDA), Australian Film and Television Academy (TAFTA), The Actors Centre (Sydney), Victorian College of the Arts (VCA), Sydney Drama School, Rogues West Acting School (Vancouver, Canada), and Royal Academy of Dramatic Art (RADA, London).</strong>
           </p>
           <p className="mb-3 lg:mb-6">
-            Acting has always been at the heart of what I do. I’ve had the opportunity to play lead, featured, supporting, and character roles across both film and television, with screen credits including <strong>Cleverman, Crownies, Behind Mansion Walls, Deadly Women, The Wedge,
-            Satisfaction, and The Librarians.</strong>
+            Throughout my career, I’ve had the privilege of working with many talented directors, producers, ADs, photographers, videographers, and production crews across the globe. I’ve appeared in a wide range of film and television productions and have commercially represented some of the world’s most recognised brands.
+          </p>
+          <p className="mb-3 lg:mb-6">
+            Acting has always been at the heart of what I do. I’ve had the opportunity to play lead, featured, supporting, and character roles across both film and television, with screen credits including <strong className="bio-strong">Cleverman, Crownies, Behind Mansion Walls, Deadly Women, The Wedge, Satisfaction, and The Librarians.</strong>
           </p>
         </div>
 
@@ -61,19 +59,22 @@ const Bio = () => {
           data-aos-delay="600"
         >
           <p className="mb-3 lg:mb-6">
-            I have also enjoyed a strong career in live television presenting—appearing on <strong>OpenShop, TV Shop,</strong> and hosting community programs and radio shows for <strong>Outlook TV, Out TV, Bent TV, and JOY 96.9FM,</strong> celebrating diverse voices within the LGBTQIA+ community.
+            I have also enjoyed a strong career in live television presenting—appearing on <strong className="bio-strong">OpenShop, TV Shop,</strong> and hosting community programs and radio shows for <strong className="bio-strong">Outlook TV, Out TV, Bent TV, and JOY 96.9FM,</strong> celebrating diverse voices within the LGBTQIA+ community.
           </p>
-          <p>
-            In recent years, I have embraced the creative potential of digital platforms and social media. Working directly with clients online has opened new doors for collaboration, storytelling, and brand partnerships, allowing me to merge my industry experience with modern content creation. I’m proud to represent a new generation of multi-skilled performers—balancing on-camera work with content creation and influencer collaborations in an evolving entertainment landscape.
-          </p>
-          <p className="mt-4">
-            Health, fitness, and wellness are core parts of who I am, both personally and professionally. I believe in maintaining a healthy lifestyle and positive mindset, and I’ve had the pleasure of working on campaigns that promote wellbeing, adventure, and self-improvement. Over the years, I’ve partnered with travel, skincare, fitness, and lifestyle brands to help inspire others to live with energy, confidence, and purpose.
+          <p className="mb-3 lg:mb-6">
+            In recent years, I have embraced the creative potential of digital platforms and social media. Working directly with clients online has opened new doors for collaboration, storytelling, and brand partnerships, allowing me to merge my industry experience with modern content creation.
           </p>
           <p className="mt-4">
-            My sense of self and adventure has taken me all around the world and back again—and what an incredible journey it’s been. I’m Australian born, Australian proud, and more motivated than ever to keep creating, collaborating, and moving forward.
+            Health, fitness, and wellness are core parts of who I am, both personally and professionally. I believe in maintaining a healthy lifestyle and positive mindset, and I’ve had the pleasure of working on campaigns that promote wellbeing, adventure, and self-improvement. My sense of self and adventure has taken me all around the world and back again—and what an incredible journey it’s been. I’m Australian born, Australian proud, and more motivated than ever to keep creating, collaborating, and moving forward.
           </p>
         </div>
       </div>
+      <style jsx>{`
+        .bio-strong {
+          font-weight: 800; /* extra bold */
+          font-size: calc(1em + 2px);
+        }
+      `}</style>
     </div>
   );
 };
